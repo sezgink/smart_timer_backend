57eefb571c0937048e04ce0146475e14bf5cc36e
sezgink <sezginkutlu00@gmail.com>
2022-11-21 22:53:53 +0300

    Interval adding WIP

--- src/routers/intervals.js
+++ src/routers/intervals.js
@@ -19,4 +19,30 @@ router.get('/intervals/:id', auth, async (req,res)=>{
     }
 });
 
+router.post('/intervals/add', auth, async (req,res)=>{
+    try{
+    const interval = new Interval({intervalLength:req.body.intervalLength, task:req.body.task,owner:req.user._id});
+
+    await interval.save();
+
+    response.send(interval);
+    } catch(e){
+        res.status(500).send();
+    }
+});
+
+router.post('/intervals/addMultiple', auth, async (req,res)=>{
+    try{
+    const interval = await Interval.findOne({_id:req.params.id, owner : req.user});
+
+    if (!interval) {
+        return res.status(404).send();
+    }
+
+    response.send(interval);
+    } catch(e){
+        res.status(500).send();
+    }
+});
+
 module.exports = router;
