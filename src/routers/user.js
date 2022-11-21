dfb74ca8789ee069de06e0d49d80d8caa1140615
sezgink <sezginkutlu00@gmail.com>
2022-11-21 20:24:47 +0300

    Letter change for api endpoits

--- src/routers/user.js
+++ src/routers/user.js
@@ -61,6 +61,37 @@ router.post('/user/login',async (req, res) => {
         res.status(500).send();
     }
   });
+  
+  router.get('/user/me', auth, async (req, res) => {
+    res.send(req.user);
+  });
+
+  router.patch('/user/me', auth, async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        updates.forEach((update) => req.user[update] = req.body[update])
+        await req.user.save()
+        res.send(req.user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+  })
+
+  router.delete('/user/me', auth, async (req, res) => {
+      try {
+          await req.user.remove();
+          res.send(req.user);
+      } catch (e) {
+          res.status(500).send();
+      }
+  })
 
 
 
